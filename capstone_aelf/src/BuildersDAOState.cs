03fc8d3ae09af6617ffdf5974bf8ebf1f3a9ef57
hidden-gm <165505859+hidden-gm@users.noreply.github.com>
2024-07-07 16:30:44 +0000

    d

--- capstone_aelf/src/BuildersDAOState.cs
+++ capstone_aelf/src/BuildersDAOState.cs
@@ -1,11 +1,17 @@
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using AElf.Sdk.CSharp.State;
+using AElf.Types;
 
 namespace AElf.Contracts.BuildersDAO
 {
     // The state class is access the blockchain state
-    public class BuildersDAOState : ContractState 
+    public class BuildersDAOState : ContractState
     {
-        // A state that holds string value
-        public StringState Message { get; set; }
+        public BoolState Initialized { get; set; }
+        public MappedState<Address, bool> Members { get; set; }
+        public MappedState<string, Proposal> Proposals { get; set; }
+        public Int32State MemberCount { get; set; }
+        public Int32State NextProposalId { get; set; }
     }
 }
